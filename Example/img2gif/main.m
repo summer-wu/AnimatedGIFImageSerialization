9c14796e0c552089dd1dca0f19c89b55c496b5a3
giveme5 <286674783@qq.com>
2016-10-31 15:58:36 +0800

    add img2gif

---
+++ Example/img2gif/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  img2gif
+//
+//  Created by n on 16/10/31.
+//  Copyright © 2016年 Mattt Thompson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
